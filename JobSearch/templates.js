29e23262c67fb63c983fcbcfacd9f2ec49072687
Estebankito7 <106397097+Estebankito7@users.noreply.github.com>
2023-09-30 17:25:50 -0500

    Update templates.js

--- JobSearch/templates.js
+++ JobSearch/templates.js
@@ -13,12 +13,13 @@ export const jobTemplate = (job, currency) =>
 <div class="card">
   <div class="card-body">
   <h3>Publicado en ${job.job_publisher}</h3>
-  <h4 class="card-title">${job.job_title}, ${job.job_max_salary !== undefined && job.job_max_salary !== null
+  <h4 class="card-title">${job.job_title}, {job.job_max_salary !== undefined && job.job_max_salary !== null
     ? `Hasta ${currency}${job.job_max_salary}`
     : (job.job_min_salary !== undefined && job.job_min_salary !== null
       ? `Desde ${currency}${job.job_min_salary}`
-      : 'Salario sin definir o en la descripción del trabajo')}
+      : '')}
   </h4>
+  <h4>Valoración de los candidatos que han aplicado:${job.job_apply_quality_score}/1</h4>
   <h5>${job.job_city}</h5>
   <p class="card-text">${cortarDescripcion(job.job_description)}</p>
   <a class="Joblink" href="${job.job_apply_link}">View Job</a>
